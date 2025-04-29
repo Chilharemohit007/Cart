0cae5f9e4529d3400de060ade681918e26246530
mohit.chilhare <chilharemohit2912@gmail.com>
2025-04-29 11:22:19 +0530

    Initial Commit

---
+++ cart/src/CartItem.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+class CartItem extends React.Component{
+    constructor (){
+        super();
+        this.state ={
+            price : 999,
+            title : 'Mobile Phone',
+            qty : 1,
+            img : ''
+        }
+
+        this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity(){
+        console.log('this', this.state);
+    }
+    render(){
+      
+        const {price, title, qty} = this.state;
+        return(
+            <div className="cart-item">
+
+                <div className="left-block">
+                    <img style={styles.image}></img>
+                </div>
+
+                <div className="right-block">
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color: '#777'}}>Rs {price}</div>
+                    <div style={{color: '#777'}}>Qty {qty}</div>
+
+                    <div className="cart-item-actions"></div>
+                    <img alt="increase" className="action-icons" onClick={this.increaseQuantity} src="https://cdn-icons-png.flaticon.com/128/11127/11127933.png"/> 
+                    <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png"/> 
+                    <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png"/> 
+                </div>
+
+            </div>
+        );
+    }
+}
+
+const styles ={
+    image: {
+        height :110,
+        width : 110,
+        borderRadius : 4,
+        background: '#ccc'
+    }
+}
+export default CartItem;
